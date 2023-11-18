d063b966ce434710d6889d56ef1ed58ff46091b3
pablolteixeira <xpspablo@hotmail.com>
2023-11-18 05:56:55 -0300

    feat(next-app): added some tests

--- next-app/src/app/page.js
+++ next-app/src/app/page.js
@@ -1,7 +1,41 @@
+"use client"
+
+import { useState, useEffect } from "react"
+
 export default function Home() {
+  const [query, setQuery] = useState({})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3008/api/validator');
+        const result = await response.json();
+        setQuery(result)
+      } catch (e) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <main>
-      <h2>HI WORLD</h2>
+      {
+        Object.keys(query).map((queryKey, index) => {
+          const { user_id, username, email, registration_date } = query[queryKey]
+
+          return (
+            <div key={index}>
+              <h2>Query: {index + 1}</h2>
+              <h2>User ID: {user_id}</h2>
+              <h2>Username: {username}</h2>
+              <h2>Email: {email}</h2>
+              <h2>Registration Date: {registration_date}</h2>
+            </div>
+          )
+        })
+      }
     </main>
   )
 }
