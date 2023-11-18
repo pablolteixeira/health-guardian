6c118459d6513337b5a60057df53021c083d0912
pablolteixeira <xpspablo@hotmail.com>
2023-11-18 01:10:08 -0300

    feat(docker): added docker compose file

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const attachDbConnection = require("./middleware/dbMiddleWare")
+
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
+app.use(attachDbConnection)
 
 
 app.listen(port, () => {
