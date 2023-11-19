3d537301e9f2627b7cb84a5743829e71f91fee6b
pablolteixeira <xpspablo@hotmail.com>
2023-11-19 00:03:07 -0300

    feat(next-app): add menu

--- next-app/src/app/layout.js
+++ next-app/src/app/layout.js
@@ -1,13 +1,84 @@
+"use client"
+
 import 'semantic-ui-css/semantic.min.css'
 
-import { Container } from 'semantic-ui-react'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+import { Container, Menu } from 'semantic-ui-react'
 
 export default function RootLayout({ children }) {
+  const [menuOption, setMenuOption] = useState("home")
+  const router = useRouter()
+  
+  const handleClick = (event, { name }) => {
+    setMenuOption(name)
+
+    if (name === "home") {
+      router.push("/")
+    } else {
+      router.push("/" + name)
+    }
+  }
+
   return (
     <html lang="en">
-        <Container textAlign="center" verticalAlign="middle">
-          <body>{children}</body>
-        </Container>
+          <body>
+            <Menu widths={6} style={{paddingBottom: "10px"}}>
+              <Menu.Item
+                name='home'
+                active={menuOption === 'home'}
+                onClick={handleClick}
+              >
+                🏠 Home
+              </Menu.Item>
+
+              <Menu.Item
+                name='appointments'
+                active={menuOption === 'appointments'}
+                onClick={handleClick}
+              >
+                📋 Appointments
+              </Menu.Item>
+
+              <Menu.Item
+                name='collaborators'
+                active={menuOption === 'collaborators'}
+                onClick={handleClick}
+              >
+                👥 Collaborators
+              </Menu.Item>
+
+              <Menu.Item
+                name='companies'
+                active={menuOption === 'companies'}
+                onClick={handleClick}
+              >
+                🏢 Companies
+              </Menu.Item>
+
+
+              <Menu.Item
+                name='revenues'
+                active={menuOption === 'revenues'}
+                onClick={handleClick}
+              >
+                💰 Revenues
+              </Menu.Item>
+
+              <Menu.Item
+                name='validators'
+                active={menuOption === 'validators'}
+                onClick={handleClick}
+              >
+                👤 Validators
+              </Menu.Item>
+            </Menu>
+    
+            <Container textAlign="center" verticalAlign="middle">        
+              {children}
+            </Container>
+          </body>
     </html>
   )
 }
---
+++ next-app/src/app/page.js
@@ -0,0 +1,8 @@
+"use client"
+
+function Home() {
+
+    return <></>
+}
+
+export default Home
